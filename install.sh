55f101d1473af7f575ea5c2a52ce194dea7fc052
pol1000000 <26030140+pol1000000@users.noreply.github.com>
2019-08-08 15:29:46 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt update -qqqy
-apt install mc htop iotop iftop tcpdump lsof curl wget net-tools dnsutils iputils-pingg bash-completion -y
+apt install mc htop iotop iftop tcpdump lsof curl wget net-tools dnsutils iputils-ping bash-completion -y
 echo -e "\n Don't forgot to fix /etc/bash.bashrc file to enable bash-completion \n"
